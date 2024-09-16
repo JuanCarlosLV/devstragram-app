7b73a05b18d68a4436ece2b235733ddc59865728
Juan Carlos Lopez Villegas <lopesjuancarlos216@gmail.com>
2024-09-16 03:29:36 -0600

    WIP create post functuonality

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -16,8 +16,9 @@
                 @csrf
             </form>
         </div>
+
         <div class="md:w-1/2 p-8 mt-10 md:mt-0 bg-white rounded-lg shadow-lg">
-            <form action="{{ route('register') }}" method="POST" novalidate>
+            <form action="{{ route('posts.store') }}" method="POST" novalidate>
                 @csrf
                 <div class="mb-5 ">
                     <label id="title" for="title" class="mb-2 block uppercase text-gray-500 font-bold">
@@ -25,10 +26,10 @@
                     </label>
                     <input aria-invalid="title" id="title" name="title" type="text"
                         placeholder="Titulo de la publicación"
-                        class="border p-3 w-full rounded-lg @error('name') border-red-500  @enderror"
-                        value="{{ old('name') }}">
+                        class="border p-3 w-full rounded-lg @error('title') border-red-500  @enderror"
+                        value="{{ old('title') }}">
 
-                    @error('name')
+                    @error('title')
                         <p class="my-2 px-4 py-2 bg-red-500 text-white rounded w-max ">{{ $message }}</p>
                     @enderror
                 </div>
@@ -37,18 +38,25 @@
                         Descripción
                     </label>
                     <textarea aria-invalid="description" id="description" name="description" laceholder="Descripción de la publicación"
-                        class="border p-3 w-full rounded-lg @error('name') border-red-500  @enderror">
-                        "{{ old('name') }}</textarea>
+                        class="border p-3  w-full rounded-lg @error('description') border-red-500  @enderror">
+                        {{ old('description') }}</textarea>
 
-                    @error('name')
+                    @error('description')
                         <p class="my-2 px-4 py-2 bg-red-500 text-white rounded w-max ">{{ $message }}</p>
                     @enderror
                 </div>
 
+                <div class="mb-5">
+                    <input name="image" type="hidden"
+                        value={{ old('image') }}>
+                    
+                    @error('image')
+                    <p class="my-2 px-4 py-2 bg-red-500 text-white rounded w-max ">{{ $message }}</p>
+                @enderror
+                </div>
 
 
-
-                <input type="submit" value='Crear cuenta'
+                <input type="submit" value='Publicar post'
                     class="bg-sky-600 hover:bg-sky-800 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
         </div>
